4b91940ed2da36620170b521684a2025a7425ba3
ambrwlsn <arw89@uclive.ac.nz>
2020-10-04 19:21:43 +0200

    Add search bar

--- data.js
+++ data.js
@@ -2,6 +2,10 @@ import DOMPurify from 'dompurify';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const term = urlParams.get('term');
+
 async function getDadJoke() {
   let response = await fetch('https://icanhazdadjoke.com/', {
     headers: {
@@ -9,6 +13,17 @@ async function getDadJoke() {
     }
   });
   let data = await response.json();
+  return data;
+}
+
+async function searchDadJokes() {
+  let response = await fetch(`https://icanhazdadjoke.com/search?term=${term}`, {
+    headers: {
+      Accept: 'application/json'
+    }
+  });
+  let data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -34,10 +49,41 @@ const insertJoke = data => {
   return DOMPurify.sanitize(joke);
 };
 
-const buttonClick = jokeButton[0].addEventListener('click', () =>
+const insertSearchedJokes = data => {
+  const jokeContainer = document.getElementsByClassName('joke-list');
+  const jokes = data.results;
+
+  if (term && jokes.length === 0) {
+    const noJokeMarkup = `<div>No jokes found! Try a different search term.</div>`
+    jokeContainer[0].insertAdjacentHTML('afterbegin', noJokeMarkup);
+  }
+
+  const listItems = jokes
+    .map(joke => {
+      return `<li>${joke.joke}</li>`;
+    })
+    .join('');
+
+  const jokeMarkup = `<ul>${listItems}</ul>`;
+
+  jokeContainer[0].insertAdjacentHTML('afterbegin', jokeMarkup);
+};
+
+jokeButton[0].addEventListener('click', () =>
   getDadJoke().then(data => {
     insertJoke(data);
   })
 );
+
+const jokeSearchButton = document
+  .getElementsByClassName('search-button')[0]
+  .addEventListener('submit', (event) => {
+    event.preventDefault();
+  });
+
+
+searchDadJokes().then(data => {
+  insertSearchedJokes(data);
+})
 
 // once the button has been pressed, disable it for 12 hours
